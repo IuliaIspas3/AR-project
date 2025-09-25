6564fe7a8e0f98d3b5212118d73c47636ffe5d0e
DominikaJanczyszyn <janczyszyndominika@gmail.com>
2025-09-25 15:31:29 +0200

    first build

---
+++ Assets/Scripts/Placer.cs
@@ -0,0 +1,29 @@
+namespace Assets.Scripts
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+    using UnityEngine.Serialization;
+    using UnityEngine.XR.ARFoundation;
+    using UnityEngine.XR.ARSubsystems;
+
+    public class Placer : PressInputBase
+    {
+        [SerializeField] private ARRaycastManager raycastManager;
+        [SerializeField] private GameObject objectToPlace;
+        private static List<ARRaycastHit> _hits = new();
+        private bool _placed;
+
+        protected override void OnPressBegan(Vector3 position)
+        {
+            base.OnPressBegan(position);
+            if (_placed) return;
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+            if (!raycastManager.Raycast(position, _hits, TrackableType.PlaneWithinPolygon)) return;
+            var hitpose = _hits[0].pose;
+            hitpose.position.y += 0.05f;
+            Instantiate(objectToPlace, hitpose.position, hitpose.rotation);
+            _placed = true;
+        }
+    }
+}
